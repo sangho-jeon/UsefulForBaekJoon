e7da5b005a1b486cf7972b0728b425beb7815d69
GitHub Action <action@github.com>
2022-08-01 06:19:41 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sanghojeon/usefulforbaekjoon/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sanghojeon.usefulforbaekjoon.services
+
+import com.github.sanghojeon.usefulforbaekjoon.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
